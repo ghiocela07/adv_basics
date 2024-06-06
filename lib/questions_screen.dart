780d6d4db6c3ac5c1f63ba317f28eae2050cc651
gherasal <alexandra.gherasim@kronsoft.ro>
2024-06-06 12:43:17 +0300

    Section 3 73

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -36,7 +36,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.answers.map((answer) {
+            ...currentQuestion.getShuffeldAnswers().map((answer) {
               return AnswerButton(
                 answerText: answer,
                 onTap: () {},
