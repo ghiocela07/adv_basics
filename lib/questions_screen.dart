954f95d14ce462566603bbfe6f571a3abacb4031
gherasal <alexandra.gherasim@kronsoft.ro>
2024-06-06 12:28:52 +0300

    Section 3- 70

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,17 +1,48 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/models/quiz_question.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/qustions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-  
+
   @override
   State<StatefulWidget> createState() {
-      return _QuestionsScreenState();
+    return _QuestionsScreenState();
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Question screen');
+    final QuizQuestion currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white, fontSize: 14),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[0],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[1],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText:  currentQuestion.answers[2],
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
